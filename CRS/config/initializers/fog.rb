ad69a9669383ec42a701e66fb3d27519ad2cdf75
EC2 Default User <kss0908k@gmail.com>
2018-11-07 23:08:15 +0000

    edit css

--- CRS/config/initializers/fog.rb
+++ CRS/config/initializers/fog.rb
@@ -2,8 +2,8 @@
      config.fog_provider = 'fog/aws'                        # required
      config.fog_credentials = {
      provider:              'AWS',                        # required
-         aws_access_key_id:     'AKIAJSFY4QPYCHL7K6HA',              # required
-         aws_secret_access_key: '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',                        # required
+         # aws_access_key_id:     'id',              # required
+         # aws_secret_access_key: 'pass',                        # required
          region:                'ap-northeast-2',             # 아시아-한국서버 명칭
          endpoint:              'https://s3-ap-northeast-2.amazonaws.com' # 아시아-한국서버
      }
