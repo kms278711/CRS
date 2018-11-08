a4cddbe9988cbd7523c99a5caad90f6eeb728efe
EC2 Default User <ec2-user@ip-172-31-27-202.us-east-2.compute.internal>
2018-11-08 03:42:32 +0000

    한글화,imagenone

--- CRS/config/initializers/fog.rb
+++ CRS/config/initializers/fog.rb
@@ -2,8 +2,8 @@
      config.fog_provider = 'fog/aws'                        # required
      config.fog_credentials = {
      provider:              'AWS',                        # required
-         # aws_access_key_id:     'id',              # required
-         # aws_secret_access_key: 'pass',                        # required
+         aws_access_key_id:     'id',              # required
+         aws_secret_access_key: 'pass',                        # required
          region:                'ap-northeast-2',             # 아시아-한국서버 명칭
          endpoint:              'https://s3-ap-northeast-2.amazonaws.com' # 아시아-한국서버
      }
