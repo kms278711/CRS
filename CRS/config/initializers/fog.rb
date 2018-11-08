99ecc7acdf4838dcf6592abdb290813534347b78
EC2 Default User <kss0908k@gmail.com>
2018-11-08 04:04:23 +0000

    upstream merge

--- CRS/config/initializers/fog.rb
+++ CRS/config/initializers/fog.rb
@@ -2,8 +2,8 @@
      config.fog_provider = 'fog/aws'                        # required
      config.fog_credentials = {
      provider:              'AWS',                        # required
-         aws_access_key_id:     'id',              # required
-         aws_secret_access_key: 'pass',                        # required
+         aws_access_key_id:     'AKIAJU5FUGH2BKGMHSHQ',              # required
+         aws_secret_access_key: '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',                        # required
          region:                'ap-northeast-2',             # 아시아-한국서버 명칭
          endpoint:              'https://s3-ap-northeast-2.amazonaws.com' # 아시아-한국서버
      }
